23f576b5e9aecaf9c5fbcc6d06166afd38719e6c
Ali Reza Fayazi <sfayazi@g.clemson.edu>
2017-02-23 16:55:36 -0500

    Add files via upload

--- 3-state_flow_machine_method#1/main.cpp
+++ 3-state_flow_machine_method#1/main.cpp
@@ -4,24 +4,6 @@
  *  Created on: Jan 27, 2017
  *  Author: S. A. Fayazi
 *
-*  The code implements the state machine shown below in C++ using std::map:
-*
-*         ------------                 	  	  -------------
-*         |  State   |						 |	  State   |
-*         |     A    |--------Event 0------->|	    B	  |
-*		  |          |						 |			  |
-*         ------------						  -------------
-*				^		    					|		^
-*				|								|    	|
-*				|							Event 1   	|
-*				|								|    Event 2
-*			Event 0								|  	 	|
-*				|								V   	|
-*				|               			----------------
-*				|							|	  State     |
-*				----------------------------|	    C	    |
-*											|			    |
-*											-----------------
 */
 
 #include "stateMachine.h"
