d08ec1dda32307c60983aac1771017848eebba77
Ali Reza Fayazi <sfayazi@g.clemson.edu>
2017-02-23 16:56:19 -0500

    Add files via upload

--- 6-state_flow_machine_method#2/main.c
+++ 6-state_flow_machine_method#2/main.c
@@ -4,24 +4,7 @@
 *  		Created on: Feb 13, 2017
 *      	Author: S. Ali Reza Fayazi
 *
-*  The code implements the state machine shown below in C:
 *
-*         ------------                 	  	  -------------
-*         |  State   |						 |	  State   |
-*         |     A    |--------Event 0------->|	    B	  |
-*		  |          |						 |			  |
-*         ------------						  -------------
-*				^		    					|		^
-*				|								|    	|
-*				|							Event 1   	|
-*				|								|    Event 2
-*			Event 0								|  	 	|
-*				|								V   	|
-*				|               			----------------
-*				|							|	  State     |
-*				----------------------------|	    C	    |
-*											|			    |
-*											-----------------
 */
 #include <stdio.h>
 #include <stdint.h>
